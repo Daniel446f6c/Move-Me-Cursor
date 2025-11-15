c33901217c07712b73bcc3e5bf5740a3aefba27c
Daniel D <68905439+Daniel446f6c@users.noreply.github.com>
2025-11-15 13:30:35 +0100

    UI and Logic refactored

--- src/main/java/com/danield/movemecursor/App.java
+++ src/main/java/com/danield/movemecursor/App.java
@@ -21,10 +21,13 @@ public class App extends Application {
     public static final double MIN_WIDTH = 230.0;
     public static final String VERSION = "0.0.2";
     public static final String ICON_PATH = "Icon.png";
+    public static final String MAIN_FXML = "PrimaryView.fxml";
+    public static final String STYLESHEET = "style.css";
 
     @Override
     public void start(Stage stage) {
         scene = new Scene(loadFXML(), MIN_WIDTH, MIN_HEIGHT);
+        scene.getStylesheets().add(loadStylesheet());
         stage.getIcons().add(loadIcon());
         stage.setTitle(String.format("%s %s", TITLE, VERSION));
         stage.setMinHeight(MIN_HEIGHT);
@@ -38,9 +41,13 @@ public class App extends Application {
         return scene;
     }
 
+    private static String loadStylesheet() {
+        return Objects.requireNonNull(App.class.getResource(STYLESHEET)).toExternalForm();
+    }
+
     private static Parent loadFXML() {
         try {
-            return new FXMLLoader(App.class.getResource("PrimaryView" + ".fxml")).load();
+            return new FXMLLoader(Objects.requireNonNull(App.class.getResource(MAIN_FXML))).load();
         } catch (IOException e) {
             System.err.println("Failed to load FXML");
             throw new RuntimeException(e);
